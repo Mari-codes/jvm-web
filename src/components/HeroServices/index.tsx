94b434a8f95c3233a8312cfac6c9bc227af9f0ee
Luiz <fellipevantine12@gmail.com>
2026-01-24 01:46:49 -0300

    feat(hero-services): add hero content with semantic text highlight

---
+++ src/components/HeroServices/index.tsx
@@ -0,0 +1,28 @@
+import { WrapperHeroSection, ContentWrapper, TitleWrapper, SubTitleWrapper, ParagraphWrapper, ParagraphBolder } from "./styles.ts";
+
+export const HeroServices = () => { 
+
+  return (
+
+    <WrapperHeroSection>
+
+      <ContentWrapper>
+        
+        <TitleWrapper>Especialistas em</TitleWrapper>
+        <SubTitleWrapper>Resolver Problemas</SubTitleWrapper>
+    
+        
+
+        <ParagraphWrapper>
+          Entendemos seu desafio, analisamos o cenário e entregamos a solução <ParagraphBolder>certa.</ParagraphBolder>
+        </ParagraphWrapper>
+
+        <ParagraphWrapper>
+         Nada de fórmulas prontas: usamos nossa experiência para desenvolver projetos sob medida, focados em performance, escalabilidade e resultado real para o seu negócio.
+        </ParagraphWrapper>
+
+      </ContentWrapper>
+
+    </WrapperHeroSection>
+  );
+};
