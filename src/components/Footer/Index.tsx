1e1471df906f6e14a6566bca9c19376f24187a8a
Walter pereira <walterpereira2015@gmail.com>
2026-01-23 23:10:51 -0300

    feat(footer): Add Footer component

---
+++ src/components/Footer/Index.tsx
@@ -0,0 +1,58 @@
+import {
+  Column,
+  Copyright,
+  CopyrightText,
+  Divider,
+  FooterWrapper,
+  LinkArea,
+  List,
+  ListItem,
+  ListTitle,
+} from './styles';
+
+function Footer() {
+  return (
+    <FooterWrapper>
+      <Divider />
+
+      <LinkArea>
+        <Column>
+          <ListTitle>Features</ListTitle>
+          <List>
+            <ListItem>Application Tour</ListItem>
+            <ListItem>Pricing Tables</ListItem>
+            <ListItem>Jobs & Careers</ListItem>
+            <ListItem>About & Developers</ListItem>
+          </List>
+        </Column>
+
+        <Column>
+          <ListTitle>Application</ListTitle>
+          <List>
+            <ListItem>Application Tour</ListItem>
+            <ListItem>Pricing Tables</ListItem>
+            <ListItem>Jobs & Careers</ListItem>
+            <ListItem>About & Developers</ListItem>
+          </List>
+        </Column>
+
+        <Column>
+          <ListTitle>Knife Theme</ListTitle>
+          <List>
+            <ListItem>Application Tour</ListItem>
+            <ListItem>Pricing Tables</ListItem>
+            <ListItem>Jobs & Careers</ListItem>
+            <ListItem>About & Developers</ListItem>
+          </List>
+        </Column>
+      </LinkArea>
+      <Copyright>
+        <CopyrightText>
+          © Copyright 2022 — All rights reserved — JVM by Kallarari
+        </CopyrightText>
+      </Copyright>
+    </FooterWrapper>
+  );
+}
+
+export default Footer;
