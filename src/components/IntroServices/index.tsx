19f69d251708210cc15b08f8858369a793d8688e
Luiz <fellipevantine12@gmail.com>
2026-02-02 19:49:39 -0300

    refactor: reorganize components and adjust spacing

--- src/components/IntroServices/index.tsx
+++ src/components/IntroServices/index.tsx
@@ -1,22 +1,47 @@
-import { ServiceCard } from "../ServicesCards"
-import { LogoSection } from "../LogoSection"
-import { HeroServices } from "../HeroServices"
+import { ServiceCard } from '../ServicesCards';
+import { LogoSection } from '../LogoSection';
+import { HeroServices } from '../HeroServices';
+import { HeroWrapper, LeftWrapper, RightWrapper, CardWrapper, InfoTitle } from './style';
 
 export const IntroServices = () => {
     return (
         <>
-            <HeroServices />
+            <HeroWrapper>
+                <LeftWrapper>
+                    <HeroServices />
+                </LeftWrapper>
 
-            <ServiceCard top left icon={'ri:whatsapp-fill'} title={'Automação de Whatsapp'} />
-
-            <ServiceCard top right icon={'material-symbols:sell'} title={'Página de vendas'} />
-
-            <ServiceCard bottom left icon={'eos-icons:ai'} title={'Automação de IA'} />
-
-            <ServiceCard bottom right icon={'mdi:web'} title={'Websites e plataformas'} />
-
+                <CardWrapper>
+                    <InfoTitle>clique para saber mais</InfoTitle>
+                    <RightWrapper>
+                        <ServiceCard
+                            top
+                            left
+                            icon={'ri:whatsapp-fill'}
+                            title={'Automação de Whatsapp'}
+                        />
+                        <ServiceCard
+                            top
+                            right
+                            icon={'material-symbols:sell'}
+                            title={'Página de vendas'}
+                        />
+                        <ServiceCard
+                            bottom
+                            left
+                            icon={'eos-icons:ai'}
+                            title={'Automação de IA'}
+                        />
+                        <ServiceCard
+                            bottom
+                            right
+                            icon={'mdi:web'}
+                            title={'Websites e plataformas'}
+                        />
+                    </RightWrapper>
+                </CardWrapper>
+            </HeroWrapper>
             <LogoSection />
-
         </>
     );
 };
